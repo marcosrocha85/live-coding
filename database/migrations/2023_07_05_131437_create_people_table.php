4340267f2c5b4761a039c55e2c92df24c426e0e2
Marcos Rocha <marcosrocha85@gmail.com>
2023-07-05 14:47:53 -0300

    Adding people table

---
+++ database/migrations/2023_07_05_131437_create_people_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->id();
+            $table->string('business_name', 200)->nullable();
+            $table->string('commercial_name', 200);
+            $table->integer('person_type')->default(1);
+            $table->string('fiscal_document', 20);
+            $table->string('telefone', 20);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('people');
+    }
+};
