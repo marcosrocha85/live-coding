e1ab93e32d386068683234e4767963549f596d44
Marcos Rocha <marcosrocha85@gmail.com>
2023-07-05 14:57:20 -0300

    Updating account plans table

--- database/migrations/2023_07_05_174917_create_account_plans_table.php
+++ database/migrations/2023_07_05_174917_create_account_plans_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('account_plans', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->enum('account_type', ['RECEIVE', 'PAY']);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
