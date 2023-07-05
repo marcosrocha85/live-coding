3748278472dfee7bef274f9a0edd7b0d657bed35
Marcos Rocha <marcosrocha85@gmail.com>
2023-07-05 15:18:33 -0300

    Adding payments table

---
+++ database/migrations/2023_07_05_181613_create_payments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBitInteger('installment_id');
+            $table->unsignedBitInteger('installment_installment_id');
+            $table->dateTime('emission_date');
+            $table->decimal('amount', $precision = 15, $scale = 4);
+            $table->decimal('fine', $precision = 15, $scale = 4);
+            $table->decimal('interest', $precision = 15, $scale = 4);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
